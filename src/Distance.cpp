c61f722db1ada5ec988eef6776d2b7fe217a1448
Gregorio Daneri <greg.daneri@gmail.com>
2025-12-02 11:36:13 +0000

    Added topic in Distances.cpp to limit boundary violations + in UI.cpp move_turtle_y and topic_callaback2

--- src/Distance.cpp
+++ src/Distance.cpp
@@ -1,7 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/msg/pose.hpp"
+#include "std_msgs/msg/float32.hpp"
 #include <iostream>
 #include <string.h>
+#include <math.h>
 
 using std::placeholders::_1;
 
@@ -10,20 +12,73 @@ class DistanceNode : public rclcpp::Node
     public:
         DistanceNode() : Node("distance_node")
         {
-            publisher_ = this->create_publisher<turtlesim::msg::Pose>("distances", 10); // to publish ditances between turtle1 and turtle2
+            publisher1_ = this->create_publisher<std_msgs::msg::Float32>("distance", 10); // To publish ditances between turtle1 and turtle2
+            publisher2_ = this->create_publisher<std_msgs::msg::Float32>("boundary_condition", 10); // To publish eventual violation of boundaries
             subscription1_ = this->create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10,std::bind(&DistanceNode::topic_callback1, this, _1));
             subscription2_ = this->create_subscription<turtlesim::msg::Pose>("turtle2/pose", 10,std::bind(&DistanceNode::topic_callback2, this, _1));
+            timer1_ = this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&DistanceNode::timer_callback1, this));
+            timer2_ = this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&DistanceNode::timer_callback2, this));
         }
     private:
         void topic_callback1(const turtlesim::msg::Pose::SharedPtr pos){
-            RCLCPP_INFO(this->get_logger(), "Turtle1 updated: %f", pos->x);
+            pos_turtle1_ = *pos;
         }
         void topic_callback2(const turtlesim::msg::Pose::SharedPtr pos){
-            RCLCPP_INFO(this->get_logger(), "Turtle2 updated: %f",pos->x);
+            pos_turtle2_ = *pos;
         }
-    rclcpp::Publisher<turtlesim::msg::Pose>::SharedPtr publisher_;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription1_;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription2_;
+        void timer_callback1(){
+            distance_.data = sqrt(pow(pos_turtle1_.x - pos_turtle2_.x,2)+pow(pos_turtle1_.y - pos_turtle2_.y,2));
+            if(pos_turtle1_.x < pos_turtle2_.x) // Turtle 2 is in front of turtle 1
+                distance_.data = distance_.data*-1.0;
+            RCLCPP_INFO(this->get_logger(), "Distance: %f",distance_.data);
+            publisher1_->publish(distance_);
+        }
+        void timer_callback2(){
+            boundary_condition_.data = 0.0;
+            if(pos_turtle1_.x >= 10.0 || pos_turtle1_.y >= 10.0 || 
+                pos_turtle1_.x <= 2.0 || pos_turtle1_.y <= 2.0){
+                if(pos_turtle1_.x >= 10.0)
+                // Boundary reached on x
+                    boundary_condition_.data = 1.1;
+                if(pos_turtle1_.x <= 2.0)
+                    // Boundary reached on x
+                    boundary_condition_.data = -1.1;
+                if(pos_turtle1_.y >= 10.0)
+                    // Boundary reached on y
+                    boundary_condition_.data = boundary_condition_.data + 0.1;
+                if(pos_turtle1_.y <= 2.0)
+                    // Boundary reached on y
+                    boundary_condition_.data = boundary_condition_.data - 0.1;
+                }
+            else if(pos_turtle2_.x >= 10.0 || pos_turtle2_.y >= 10.0 || 
+                    pos_turtle2_.x <= 2.0 || pos_turtle2_.y <= 2.0){
+                if(pos_turtle2_.x >= 10.0)
+                    // Boundary reached on x
+                    boundary_condition_.data = 2.1;
+                if(pos_turtle2_.x <= 2.0)
+                    // Boundary reached on x
+                    boundary_condition_.data = -2.1;
+                if(pos_turtle2_.y >= 10.0)
+                    // Boundary reached on y
+                    boundary_condition_.data = boundary_condition_.data + 0.2;
+                if(pos_turtle2_.y <= 2.0)
+                    // Boundary reached on y
+                    boundary_condition_.data = boundary_condition_.data - 0.2;
+                }
+            else
+                boundary_condition_.data = 0.0;
+            publisher2_->publish(boundary_condition_);
+        }
+        rclcpp::TimerBase::SharedPtr timer1_;
+        rclcpp::TimerBase::SharedPtr timer2_;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher1_;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher2_;
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription1_;
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription2_;
+        std_msgs::msg::Float32 distance_;
+        std_msgs::msg::Float32 boundary_condition_;
+        turtlesim::msg::Pose pos_turtle1_;
+        turtlesim::msg::Pose pos_turtle2_;
 };
 
 int main(int argc, char **argv)
--- src/UI.cpp
+++ src/UI.cpp
@@ -1,7 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "turtlesim/msg/pose.hpp"
 #include <iostream>
 #include <string.h>
+#include <math.h>
+
+#define threshold 2.0
+
+using std::placeholders::_1;
 
 class UINode : public rclcpp::Node
 {
@@ -10,27 +17,68 @@ class UINode : public rclcpp::Node
         {
             publisher1_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10); // creates publisher on topic 'turtle1/cmd_vel'
             publisher2_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 10); // creates publisher on topic 'turtle2/cmd_vel'
+            subscription1_ = this->create_subscription<std_msgs::msg::Float32>("distance", 10,std::bind(&UINode::topic_callback1, this, _1));
+            subscription2_ = this->create_subscription<std_msgs::msg::Float32>("boundary_condition", 10,std::bind(&UINode::topic_callback2, this, _1));
             while(1){
                 user_input();
-                move_turtle();
+                if(n_robot_ == 0)
+                    exit(EXIT_SUCCESS);
+                move_turtle_x();
+                move_turtle_y();
             }
         }
     private:
         void user_input(){
-            n_robot_ = 0; vel_ = 0;
-            std::cout << "Select a robot to move it (type 1 or 2):\n";
+            n_robot_ = 0; vel_x_ = 0.0; vel_y_ = 0.0;
+            std::cout << "Select a robot to move it (type 1 or 2) or type 0 to quit:\n";
             std::cin >> n_robot_;
-            while(n_robot_ != 1 && n_robot_ != 2){
-                std::cout << "Invalid number, choose again: \n";
-                std::cin >> n_robot_;
-            }
-            std::cout << "Insert speed: \n";
-            std::cin >> vel_;
-            
+            if(n_robot_ == 0)
+                return;
+            std::cout << "Insert linear speed on x: \n";
+            std::cin >> vel_x_;
+            std::cout << "Insert linear speed on y: \n";
+            std::cin >> vel_y_;
             return;
         }
-        void move_turtle(){
-            turtle_twist_.linear.x = vel_;
+        void move_turtle_x(){
+            turtle_twist_.linear.x = vel_x_;
+            if(n_robot_ == 1)
+                publisher1_->publish(turtle_twist_);
+            else
+                publisher2_->publish(turtle_twist_);
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(1000),std::bind(&UINode::timer_callback, this));
+        }
+        void move_turtle_y(){
+            /*RCLCPP_INFO(this->get_logger(), "Distance: %f, abs distance: %f", distance_, fabs(distance_));
+            if(fabs(distance_) < threshold){
+                // Control before moving
+                if(distance_ <= 0){
+                    if((n_robot_ == 1 && vel_ > 0) || (n_robot_ == 2 && vel_ < 0))
+                        std::cout << "Turtles too close, choose another turtle or change velocity\n";
+                    else{
+                        turtle_twist_.linear.y = vel_y_;
+                        if(n_robot_ == 1)
+                            publisher1_->publish(turtle_twist_);
+                        else
+                            publisher2_->publish(turtle_twist_);
+                        timer_ = this->create_wall_timer(std::chrono::milliseconds(1000),std::bind(&UINode::timer_callback, this));
+                    }
+                }
+                else{
+                    if((n_robot_ == 1 && vel_ < 0) || (n_robot_ == 2 && vel_ > 0))
+                        std::cout << "Turtles too close, choose another turtle or change velocity\n";
+                    else{
+                        turtle_twist_.linear.y = vel_y_;
+                        if(n_robot_ == 1)
+                            publisher1_->publish(turtle_twist_);
+                        else
+                            publisher2_->publish(turtle_twist_);
+                        timer_ = this->create_wall_timer(std::chrono::milliseconds(1000),std::bind(&UINode::timer_callback, this));
+                    }
+                }
+            }
+            else{*/
+            turtle_twist_.linear.y = vel_y_;
             if(n_robot_ == 1)
                 publisher1_->publish(turtle_twist_);
             else
@@ -39,18 +87,59 @@ class UINode : public rclcpp::Node
         }
         void timer_callback(){
             turtle_twist_.linear.x = 0.0;
+            turtle_twist_.linear.y = 0.0;
             publisher1_->publish(turtle_twist_);
             publisher2_->publish(turtle_twist_);
             RCLCPP_INFO(this->get_logger(), "Stopping the turtle");
             timer_->cancel();
             return;
         }
+        void topic_callback1(const std_msgs::msg::Float32::SharedPtr dis){
+            // issue: understanding along which direction a turtle cannot move
+            distance_ = dis->data;
+            if(fabs(dis->data) < threshold){
+                if(dis->data <= 0)
+                // Turtle 1 
+                turtle_twist_.linear.x = 0.0;
+                publisher1_->publish(turtle_twist_);
+                publisher2_->publish(turtle_twist_);
+                RCLCPP_INFO(this->get_logger(), "Stopping the turtle");
+                std::cout << "Turtle stopped (too close)\n";
+                timer_->cancel();
+            }
+        }
+
+        void topic_callback2(const std_msgs::msg::Float32::SharedPtr boundary_cond){
+            if((boundary_cond->data == 1.1 || boundary_cond->data == 1.0 || boundary_cond->data == 1.2)
+                && vel_x_ > 0)
+                // Turtle1 reached right border x
+                turtle_twist_.linear.x = 0.0;
+            else if((boundary_cond->data == -1.1 || boundary_cond->data == -1.0 || boundary_cond->data == -1.2)
+                && vel_x_ < 0)
+                // Turtle1 reached left border x
+                turtle_twist_.linear.x = 0.0;
+            if((boundary_cond->data == 1.2 || boundary_cond->data == -1.0 || boundary_cond->data == 0.1 )
+                && vel_y_ > 0)
+                // Turtle1 reached upper border y
+                turtle_twist_.linear.y = 0.0;
+            else if((boundary_cond->data == 1.0 || boundary_cond->data == -1.2 || boundary_cond->data == -0.1 )
+                && vel_y_ < 0)
+                // Turtle1 reached upper border y
+                turtle_twist_.linear.y = 0.0;
+            publisher1_->publish(turtle_twist_);
+            publisher2_->publish(turtle_twist_);
+            RCLCPP_INFO(this->get_logger(), "Stopping the turtle: border reached");
+            std::cout << "Stopping the turtle: border reached\n";
+            timer_->cancel();
+        }
 
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher1_;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher2_;
+        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr subscription1_;
+        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr subscription2_;
         geometry_msgs::msg::Twist turtle_twist_;
-        int n_robot_, vel_;
+        int n_robot_; double vel_x_, vel_y_, distance_;
 };
 
 int main(int argc, char **argv)
